5e349edd21f8307d4cfe1f6bd98e8bd0ab9b98e1
Noe <noeziadi@outlook.com>
2026-01-23 10:41:58 +0100

    Test

--- src/ms-notifications/ms-notifications.controller.ts
+++ src/ms-notifications/ms-notifications.controller.ts
@@ -10,7 +10,7 @@ export class NotificationsController {
   handleUserCreated(@Payload() data: any, @Ctx() context: RmqContext) {
     console.log(`⚡️ Event Reçu via RabbitMQ :`, data);
     
-    // Exemple : Créer la notif automatiquement
+    // Exemple : Créer la notif
     if (data.user_id && data.email) {
         this.notificationsService.create(data.user_id, `Bienvenue ${data.email} !`);
     }
